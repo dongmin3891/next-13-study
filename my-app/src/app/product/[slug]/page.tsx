c95f18f8e3226ecea4648ad1ec56e5f10b42039e
dongmin3891 <rlaehdals753@naver.com>
2023-05-20 16:05:52 +0900

    feat: ssr dynamic routing

--- my-app/src/app/product/[slug]/page.tsx
+++ my-app/src/app/product/[slug]/page.tsx
@@ -9,3 +9,20 @@ export default function PantsPage({params} : Props) {
     <h1>{params.slug}PantsPage</h1>
   )
 }
+
+
+/**
+ *   dynamic routing 2
+ *  return map 함수 주의
+ *   - 기본 형태
+ *  test.map(() => {})
+ *   - dynamic routing 형태..?
+ *  test.map(() => ({}))
+ *  동적 라우팅을 아래와 같이 지정하면 해당 라우팅도 SSR로 동작을 함.
+ * */ 
+export function generateStaticParams() {
+  const products = ['skirt', 'pants'];
+  return products.map((product: string) => ({
+    slug: product
+  }))
+}
